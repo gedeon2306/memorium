0929ef03332860986d5acf012e836db345b3ce42
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-04-08 23:54:20 -1200

    modif nav

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -40,7 +40,7 @@ export default function Navbar({
 }: NavbarProps) {
   const { theme, changeTheme, themes } = useTheme();
   const [isFullscreen, setIsFullscreen] = useState(false);
-  const [notifications] = useState(9);
+  const [notifications] = useState(2);
 
   const toggleFullscreen = () => {
     if (!document.fullscreenElement) {
