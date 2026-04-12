98854ca3d94b5753d5111f177a571e5a7d7b1ae9
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-04-12 16:06:16 +0100

    Installation de jose pour décoder le token et modification de la middleware pour vérifier le role de l'utilisateur

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,26 +1,45 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { ROUTES } from '@/constants/routes';
+import { jwtVerify } from 'jose';
 
-export function middleware(request: NextRequest) {
-    const token = request.cookies.get('access_token');
+export async function middleware(request: NextRequest) {
+    const token = request.cookies.get('access_token')?.value;
     const { pathname } = request.nextUrl;
 
-    // Listes des routes pour simplifier les conditions
+    // Définition des zones
     const isAuthRoute = Object.values(ROUTES.AUTH).includes(pathname as any);
-    const isDashboardRoute = pathname.startsWith('/dashboard');
+    const isDashboardRoute = pathname.startsWith(ROUTES.DASHBOARD.ROOT);
+    const isUsersAdminPage = pathname === ROUTES.DASHBOARD.USERS;
 
-    // 1. Redirection si NON connecté : accès au dashboard interdit
-    if (!token && isDashboardRoute) {
+    let payload: any = null;
+
+    // 1. Tentative de décodage et vérification du token
+    if (token) {
+        try {
+            const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+            const { payload: verifiedPayload } = await jwtVerify(token, secret);
+            payload = verifiedPayload;
+        } catch (error) {
+            payload = null;
+        }
+    }
+
+    // 2. Protection globale du Dashboard (Redirection si non connecté)
+    if (!payload && isDashboardRoute) {
         return NextResponse.redirect(new URL(ROUTES.AUTH.LOGIN, request.url));
     }
 
-    // 2. Redirection si DÉJÀ connecté : accès aux pages d'auth interdit
-    if (token && isAuthRoute) {
+    // 3. Protection spécifique : Page Utilisateurs (Réservée aux Administrateurs)
+    if (isUsersAdminPage && payload?.role !== 'Administrateur') {
+        return NextResponse.redirect(new URL(ROUTES.DASHBOARD.ROOT, request.url));
+    }
+
+    // 4. Redirection si DÉJÀ connecté (Empêche d'aller sur Login/Register)
+    if (payload && isAuthRoute) {
         if (pathname === ROUTES.AUTH.FORGOT_PASSWORD || pathname === ROUTES.AUTH.RESET_PASSWORD) {
             return NextResponse.redirect(new URL(ROUTES.DASHBOARD.PROFIL, request.url));
-        }
-        
+        } 
         return NextResponse.redirect(new URL(ROUTES.DASHBOARD.ROOT, request.url));
     }
 
